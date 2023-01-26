b288fcee8fa289939a650860c4092767ad5b9937
Towfikrahman <tarekrahmantowfik@gmail.com>
2023-01-26 17:43:20 +0600

    repete

---
+++ Mahfuz's Treat.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int re = n * ( n + 1 )/2;
+    cout<<re;
+    return 0;
+}
--- Sad Mashuk.cpp
+++ Sad Mashuk.cpp
@@ -1,3 +1,16 @@
+Santa is sad today, and to cure his sadness, he has decided to gamble his troubles away in a game of dice.
+The casino attendant rolls a huge 1000-sided dice which has numbers from 1 to 1000
+1000; if the number that comes up on top has only odd digits, the player wins, otherwise, the house wins.
+Santa is naturally happier when winning at games of chance, and he becomes happy if and only if the number
+n that is rolled is a winning number for him. Given n, will Santa be happy?
+
+Input
+The first line of input will contain n(1 ≤ n ≤ 1000), the number that Santa rolled.
+
+Output
+Output "HAPPY" if Santa will become happy, and "SAD" if Santa stays sad.
+
+
 #include<iostream>
 using namespace std;
 int main() {
